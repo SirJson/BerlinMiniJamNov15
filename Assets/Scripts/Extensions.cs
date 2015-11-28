3265259b97fffd3d188184527a2846bd80258c38
Raffael Zica <raffael@pixeltastic.de>
2015-11-28 13:48:24 +0100

    Utility import

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+static class Extensions
+{
+	public static Rect ToUVCoords(this Rect src, float width, float height, float tiling)
+	{
+		var stepX = 1.0f / width;
+		var stepY = 1.0f / height;
+
+		return new Rect((src.x * stepX) / tiling, (1.0f - (src.y * stepY + src.height * stepY)) / tiling, (src.width * stepX) / tiling, (src.height * stepY) / tiling);
+	}
+
+	public static void SetMetallicTexture(this Material mat, Texture tex)
+	{
+		mat.SetTexture("_MetallicGlossMap", tex);
+	}
+
+	public static void SetSmoothness(this Material mat, float val)
+	{
+		mat.SetFloat("_Glossiness",val);
+	}
+
+	public static void SetAlbedoTexture(this Material mat, Texture tex)
+	{
+		mat.SetTexture("_MainTex", tex);	
+	}
+
+	public static void SetAlbedoColor(this Material mat, Color color)
+	{
+		mat.SetColor("_Color", color);
+	}
+
+	public static DateTime StartOfDay(this DateTime date)
+	{
+		return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, 0);
+	}
+
+	public static string NormalizeLineEndings(this string str)
+	{
+		return Regex.Replace(str, @"\r\n|\n\r|\n|\r", System.Environment.NewLine);
+	}
+}
---
+++ Assets/Scripts/SimpleSound.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using PathologicalGames;
+
+public class SimpleSound : MonoBehaviour
+{
+    private AudioSource source;
+    private bool running = false;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        source.spatialBlend = 0;
+        source.playOnAwake = false;
+    }
+
+    public void Play(AudioClip clip, float volume = 1.0f)
+    {
+        if(source == null) source = GetComponent<AudioSource>();
+        source.clip = clip;
+        source.volume = volume;
+        source.Play();
+        running = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(running)
+        {
+            if(!source.isPlaying) {
+                running = false;
+                PoolManager.Pools[PoolIdentifier.Audio].Despawn(transform);
+            }
+        }
+	}
+}
