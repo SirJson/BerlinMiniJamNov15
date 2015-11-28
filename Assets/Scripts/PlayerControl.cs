6e625db02dd7e3ba8172ecf9e5cca6ad074fa815
dinesh punni <dinesh.punni@hotmail.de>
2015-11-28 15:59:39 +0100

    aaa

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+
+	public float speed;
+	public Rigidbody2D player;
+	public GameObject bullet;
+
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+		PlayerControls();
+	
+		if(Input.GetKeyDown(KeyCode.Space)){
+			shoot();
+		}
+
+	}
+
+	
+	void PlayerControls(){
+
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+
+			player.velocity = Vector3.right * speed;
+		}
+
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			
+			player.velocity = Vector3.left * speed;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			
+			player.velocity = Vector3.up * speed;
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			
+			player.velocity = Vector3.down * speed;
+		}
+
+	}
+
+	void shoot(){
+
+		Instantiate(bullet, player.position , Quaternion.identity);
+	}
+
+
+
+
+}// eoc PlayerControl
+
+
